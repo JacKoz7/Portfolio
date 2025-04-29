27b10bbc76f42163c371031fe88117993f258976
Jacek <jacuskozlowski@gmail.com>
2025-04-29 08:20:18 +0200

    adding icons

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,79 @@
+import SectionHeader from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StaraIcon from "@/assets/icons/star.svg";
+import bookImage from "@/assets/images/book-cover.png";
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLicon from "@/assets/icons/html5.svg";
+import CSSicon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import TypescriptIcon from "@/assets/icons/typescript.svg";
+import DockerIcon from "@/assets/icons/docker.svg";
+import { TechIcon } from "@/components/TechIcon";
+
+const toolboxItems = [
+  {
+    title: "Javascript",
+    iconType: JavascriptIcon,
+  },
+  {
+    title: "HTML5",
+    iconType: HTMLicon,
+  },
+  {
+    title: "CSS3",
+    iconType: CSSicon,
+  },
+  {
+    title: "React",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Typescript",
+    iconType: TypescriptIcon,
+  },
+  {
+    title: "Docker",
+    iconType: DockerIcon,
+  },
+];
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div>
+      <SectionHeader
+        eyebrow="About me"
+        title="A Glimpse Into My World"
+        description="Learn more about who I am, what I do and what inspires me"
+      />
+      <div>
+        <Card>
+          <div>
+            <StaraIcon />
+            <h3>My Reads</h3>
+            <p>Explore the books shaping my perspactives.</p>
+          </div>
+          <Image src={bookImage} alt="book cover" />
+        </Card>
+        <Card>
+          <div>
+            <StaraIcon />
+            <h3>My Toolbox</h3>
+            <p>
+              Explore the technologies and tools I use to craft exceptional
+              digital experiences.
+            </p>
+          </div>
+          <div>
+            {toolboxItems.map(item =>(
+              <div key={item.title}>
+                <TechIcon component={item.iconType}/>
+                <span>{item.title}</span>
+              </div>
+            ))}
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 };
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,8 @@ import grainImage from "@/assets/images/grain.jpg";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRight from "@/assets/icons/arrow-up-right.svg";
 import Image from "next/image";
+import SectionHeader from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -49,30 +51,17 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <div className="flex justify-center">
-          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent text-center">
-            Real-World Results
-          </p>
-        </div>
-
-        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
-          Featured Projects
-        </h2>
-        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
-          See how i transformed concepts into engaging digital experiences.
-        </p>
+        <SectionHeader
+          eyebrow="Real-World Results"
+          title="Featured Projects"
+          description="See how i transformed concepts into engaging digital experiences."
+        />
         <div className="flex flex-col gap-20  mt-10 md:mt-20">
           {portfolioProjects.map((project) => (
-            <div
+            <Card
               key={project.title}
-              className="bg-gray-800 pt-8 pr-8 pl-8 rounded-3xl relative overflow-hidden z-0 after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none md:pt-12 md:px-10 lg:pt-16 lg:px-20"
+              className=" pt-8 px-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
             >
-              <div
-                className="absolute inset-0 -z-10 opacity-5"
-                style={{
-                  backgroundImage: `url(${grainImage.src})`,
-                }}
-              ></div>
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
                   <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text ">
@@ -110,7 +99,7 @@ export const ProjectsSection = () => {
                   />
                 </div>
               </div>
-            </div>
+            </Card>
           ))}
         </div>
       </div>
